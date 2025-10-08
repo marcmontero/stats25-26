83f7119e9211b0f9b5e09ed1380f3006bb29685a
Marc Montero Estopiña <marcmonteroestopina@Ordenador-portatil-de-Marc.local>
2025-10-08 20:42:52 +0200

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -440,7 +440,7 @@ const App = () => {
           alt="AE Badalonès" 
           className="club-logo"
         />
-        <h1>Estadístiques AE Badalonès 2024-2025</h1>
+        <h1>Estadístiques AE Badalonès 2025-2026</h1>
         
         <div className="teams-grid">
           {Object.entries(availableTeams).map(([teamId, team]) => (
