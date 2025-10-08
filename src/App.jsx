a745d96c76cc537637849c8e19af8114a7180ef4
Marc Montero Estopiña <marcmonteroestopina@Ordenador-portatil-de-Marc.local>
2025-10-08 21:26:40 +0200

    Fixes profile pic

--- src/App.jsx
+++ src/App.jsx
@@ -490,8 +490,8 @@ const App = () => {
                 src={currentUser.profileImage} 
                 alt={currentUser.name}
                 style={{
-                  width: '60px',
-                  height: '60px',
+                  width: '100px',
+                  height: '100px',
                   borderRadius: '50%',
                   objectFit: 'cover',
                   border: '3px solid #c41230',
