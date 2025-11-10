804d1a62dbf050b531937252562f64ba5bc40c0c
Marc Montero Estopiña <marcmonteroestopina@Ordenador-portatil-de-Marc.local>
2025-11-10 12:26:43 +0100

    Jornada 6

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,8 @@ const TEAMS_CONFIG = {
       'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/68e2bd419163a800012e059a?currentSeason=true',
       'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/68ebe2472ea75e0001d07eb8?currentSeason=true',
       'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/68f4cbdb454224000114bbdb?currentSeason=true',
-      'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/68fe650b1497f20001890556?currentSeason=true'
+      'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/68fe650b1497f20001890556?currentSeason=true',
+      'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/69106658fa539d0001a25409?currentSeason=true'
     ]
   }
 };
