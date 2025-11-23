6b3e145f914a27c5681313d226377ddad063e76f
Marc Montero Estopiña <marcmonteroestopina@Ordenador-portatil-de-Marc.local>
2025-11-23 18:42:24 +0100

    Jornada 8

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,8 @@ const TEAMS_CONFIG = {
       'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/68f4cbdb454224000114bbdb?currentSeason=true',
       'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/68fe650b1497f20001890556?currentSeason=true',
       'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/69106658fa539d0001a25409?currentSeason=true',
-      'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/6919fbed5ac4ad0001776fed?currentSeason=true'
+      'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/6919fbed5ac4ad0001776fed?currentSeason=true',
+      'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/692332c05ac4ad0001790807?currentSeason=true'
     ]
   }
 };
