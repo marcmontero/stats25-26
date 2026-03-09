8922ec0ab484190ded04387f2c7a674e976ef2ab
Marc Montero Estopiña <marcmonteroestopina@Ordenador-portatil-de-Marc.local>
2026-03-09 19:33:24 +0100

    Jornada Senior FEM MARISTES

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,8 @@ const TEAMS_CONFIG = {
       'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/6963e98d2264680001925b0a?currentSeason=true',
       'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/69750d642baa680001617330?currentSeason=true',
       'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/6988d08ef9430b000196a5b8?currentSeason=true',
-      'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/69920f374d61c500019241ff?currentSeason=true'
+      'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/69920f374d61c500019241ff?currentSeason=true',
+      'https://msstats.optimalwayconsulting.com/v1/fcbq/getJsonWithMatchStats/69add7b82b84b00001f00f39?currentSeason=true'
     ]
   }
 };
